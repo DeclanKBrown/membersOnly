d80c5a982a2d0a3e598b46f64a1b8e9601f0c248
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-12 14:55:33 +1100

    created layout and header + footer components

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,7 +1,13 @@
 export default function Header() {
     return (
-        <>
-            
-        </>
+        <header className='flex justify-between px-60 py-5 border-b border-gray-700'>
+            <div>
+                <h1 className="text-2xl font-extrabold text-sky-500">Members Only</h1>
+            </div>
+            <div className="flex gap-8">
+                <button className="px-4 py-2 rounded bg-sky-500 hover:opacity-80">Log In</button>
+                <button className="px-4 py-2 rounded bg-sky-500 hover:opacity-80">Sign Up</button>
+            </div>
+        </header>
     )
 }
--- web/src/layouts/Layout.jsx
+++ web/src/layouts/Layout.jsx
@@ -1,7 +1,12 @@
-export default function Layout() {
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+export default function Layout({ children }) {
     return (
         <>
-            
+        <Header></Header>
+            <main className='h-[calc(100vh-8rem)]'>{children}</main>
+        <Footer></Footer>
         </>
     )
 }
