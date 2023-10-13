bb7f520e7fe6482782d2172ff0280a829cb232b4
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-13 14:52:56 +1100

    Fetching messages to homepage working

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Home() {
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/all-posts')
+                setMessages(response.data.allPosts)
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <>
-            
-        </>
+        <main className="flex flex-col w-full items-center">
+            <h1 className="mt-7 mb-14 text-2xl font-bold">Messages</h1>
+            {(messages.length > 0) ? (
+                <>
+
+                </>) : (
+                    <h3> No messages yet</h3>
+                )}
+        </main>
     )
 }
