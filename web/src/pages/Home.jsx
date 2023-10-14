3f5a3e9e1612dfbf4b77aaa86cfb7c7125f51ea5
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-10-14 16:27:27 +1100

    Conditionally render add message button

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -9,6 +9,7 @@ export default function Home({ setModalMessage }) {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:3000/all-posts')
+                console.log(response)
                 setMessages(response.data.allPosts)
             } catch (error) {
                 console.error('Error fetching data:', error)
@@ -18,10 +19,22 @@ export default function Home({ setModalMessage }) {
         fetchData();
     }, []);
 
+    const [userDetails, setUserDetails] = useState(null)
+    const userStorage = localStorage.getItem('user')
+
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        if (userDetails) return
+        setUserDetails(JSON.parse(userStorage))
+    },[userDetails, userStorage])
+
     return (
         <main className="flex flex-col w-full items-center">
-            <h1 className="mt-7 mb-5 text-2xl font-bold">Messages</h1>
-            <button onClick={() => setModalMessage(true)} className="px-4 py-2 rounded bg-sky-500 hover:opacity-80 mb-10">+ Message</button>
+        <h1 className="mt-7 mb-5 text-2xl font-bold">Messages</h1>
+            {(token && userDetails) && (
+                <button onClick={() => setModalMessage(true)} className="px-4 py-2 rounded bg-sky-500 hover:opacity-80 mb-10">+ Message</button>
+            )}
             {messages.length > 0 ? (
                 messages.map((message) => (
                     <Message
